4d937fb28ebd3b219fa6c265ccc0de0a59c298c7
shahzad1999 <shahzad1999@hotmail.com>
2022-03-29 02:07:15 +0800

    Updated file reader function
    Manage to store variables into 2D arrays.

---
+++ RenTogether/fileReader.cpp
@@ -0,0 +1,214 @@
+#include <QJsonDocument>
+#include <QJsonParseError>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QJsonArray>
+#include <QObject>
+#include <QFile>
+#include <iostream>
+#include <QDebug>
+#include <QTextStream>
+#include <QVector>
+
+QVector<QStringList> electricBike{{}, {}, {}};
+QVector<QStringList> electricCar{{}, {}, {}};
+QVector<QStringList> gasBike{{}, {}, {}};
+QVector<QStringList> gasCar{{}, {}, {}};
+QVector<QStringList> hybridBike{{}, {}, {}};
+QVector<QStringList> hybridCar{{}, {}, {}};
+QVector<QStringList> user{{}, {}};
+
+int readJson()
+{
+    int i = 0;
+    QFile inFile("C:/Users/shahz/Desktop/RenTogether/electricBike.json");
+    inFile.open(QIODevice::ReadOnly | QIODevice::Text);
+    QByteArray data = inFile.readAll();
+    inFile.close();
+
+
+    QJsonParseError errorPtr;
+    QJsonDocument doc = QJsonDocument::fromJson(data, &errorPtr);
+    if (doc.isNull()) {
+       qDebug() << "Parse failed";
+   }
+    QJsonObject rootObj = doc.object();
+    QJsonArray ptsArray = rootObj.value("electricBike").toArray();
+
+
+    foreach(const QJsonValue & val, ptsArray){
+        electricBike[i].append(val.toObject().value("vehicleID").toString());
+        electricBike[i].append(val.toObject().value("brand").toString());
+        electricBike[i].append(val.toObject().value("model").toString());
+        electricBike[i].append(val.toObject().value("basePrice").toString());
+        electricBike[i].append(val.toObject().value("insuranceCoverage").toString());
+        electricBike[i].append(val.toObject().value("pricePerCharge").toString());
+        electricBike[i].append(val.toObject().value("batteryLife").toString());
+        qDebug() << electricBike[i];
+        i++;
+
+    }
+
+    QFile inFile2("C:/Users/shahz/Desktop/RenTogether/electricCar.json");
+    inFile2.open(QIODevice::ReadOnly | QIODevice::Text);
+    data = inFile2.readAll();
+    inFile2.close();
+
+
+    doc = QJsonDocument::fromJson(data, &errorPtr);
+    if (doc.isNull()) {
+       qDebug() << "Parse failed";
+   }
+    rootObj = doc.object();
+    ptsArray = rootObj.value("electricCar").toArray();
+
+    i = 0;
+    foreach(const QJsonValue & val, ptsArray){
+        electricCar[i].append(val.toObject().value("vehicleID").toString());
+        electricCar[i].append(val.toObject().value("brand").toString());
+        electricCar[i].append(val.toObject().value("model").toString());
+        electricCar[i].append(val.toObject().value("basePrice").toString());
+        electricCar[i].append(val.toObject().value("seaterNumber").toString());
+        electricCar[i].append(val.toObject().value("insuranceCoverage").toString());
+        electricCar[i].append(val.toObject().value("pricePerCharge").toString());
+        electricCar[i].append(val.toObject().value("batteryLife").toString());
+        qDebug() << electricCar[i];
+        i++;
+    }
+
+    QFile inFile3("C:/Users/shahz/Desktop/RenTogether/gasBike.json");
+    inFile3.open(QIODevice::ReadOnly | QIODevice::Text);
+    data = inFile3.readAll();
+    inFile.close();
+
+
+    doc = QJsonDocument::fromJson(data, &errorPtr);
+    if (doc.isNull()) {
+       qDebug() << "Parse failed";
+   }
+    rootObj = doc.object();
+    ptsArray = rootObj.value("gasBike").toArray();
+
+    i = 0;
+    foreach(const QJsonValue & val, ptsArray){
+        gasBike[i].append(val.toObject().value("vehicleID").toString());
+        gasBike[i].append(val.toObject().value("brand").toString());
+        gasBike[i].append(val.toObject().value("model").toString());
+        gasBike[i].append(val.toObject().value("basePrice").toString());
+        gasBike[i].append(val.toObject().value("insuranceCoverage").toString());
+        gasBike[i].append(val.toObject().value("kmPerLitre").toString());
+        qDebug() << gasBike[i];
+        i++;
+    }
+
+    QFile inFile4("C:/Users/shahz/Desktop/RenTogether/gasCar.json");
+    inFile4.open(QIODevice::ReadOnly | QIODevice::Text);
+    data = inFile4.readAll();
+    inFile4.close();
+
+
+    doc = QJsonDocument::fromJson(data, &errorPtr);
+    if (doc.isNull()) {
+       qDebug() << "Parse failed";
+   }
+    rootObj = doc.object();
+    ptsArray = rootObj.value("gasCar").toArray();
+
+    i = 0;
+    foreach(const QJsonValue & val, ptsArray){
+        gasCar[i].append(val.toObject().value("vehicleID").toString());
+        gasCar[i].append(val.toObject().value("brand").toString());
+        gasCar[i].append(val.toObject().value("model").toString());
+        gasCar[i].append(val.toObject().value("basePrice").toString());
+        gasCar[i].append(val.toObject().value("seaterNumber").toString());
+        gasCar[i].append(val.toObject().value("insuranceCoverage").toString());
+        gasCar[i].append(val.toObject().value("kmPerLitre").toString());
+        qDebug() << gasCar[i];
+        i++;
+    }
+
+
+    QFile inFile5("C:/Users/shahz/Desktop/RenTogether/hybridBike.json");
+    inFile5.open(QIODevice::ReadOnly | QIODevice::Text);
+    data = inFile5.readAll();
+    inFile5.close();
+
+
+    doc = QJsonDocument::fromJson(data, &errorPtr);
+    if (doc.isNull()) {
+       qDebug() << "Parse failed";
+   }
+    rootObj = doc.object();
+    ptsArray = rootObj.value("hybridBike").toArray();
+
+    i =0;
+    foreach(const QJsonValue & val, ptsArray){
+        hybridBike[i].append(val.toObject().value("vehicleID").toString());
+        hybridBike[i].append(val.toObject().value("brand").toString());
+        hybridBike[i].append(val.toObject().value("model").toString());
+        hybridBike[i].append(val.toObject().value("basePrice").toString());
+        hybridBike[i].append(val.toObject().value("insuranceCoverage").toString());
+        hybridBike[i].append(val.toObject().value("pricePerCharge").toString());
+        hybridBike[i].append(val.toObject().value("batteryLife").toString());
+        hybridBike[i].append(val.toObject().value("kmPerLitre").toString());
+        hybridBike[i].append(val.toObject().value("electricPriceModifier").toString());
+        hybridBike[i].append(val.toObject().value("gasPriceModifier").toString());
+        qDebug() << hybridBike[i];
+        i++;
+    }
+
+    QFile inFile6("C:/Users/shahz/Desktop/RenTogether/hybridCar.json");
+    inFile6.open(QIODevice::ReadOnly | QIODevice::Text);
+    data = inFile6.readAll();
+    inFile6.close();
+
+
+    doc = QJsonDocument::fromJson(data, &errorPtr);
+    if (doc.isNull()) {
+       qDebug() << "Parse failed";
+   }
+    rootObj = doc.object();
+    ptsArray = rootObj.value("hybridCar").toArray();
+
+    i = 0;
+    foreach(const QJsonValue & val, ptsArray){
+        hybridCar[i].append(val.toObject().value("vehicleID").toString());
+        hybridCar[i].append(val.toObject().value("brand").toString());
+        hybridCar[i].append(val.toObject().value("model").toString());
+        hybridCar[i].append(val.toObject().value("basePrice").toString());
+        hybridCar[i].append(val.toObject().value("seaterNumber").toString());
+        hybridCar[i].append(val.toObject().value("insuranceCoverage").toString());
+        hybridCar[i].append(val.toObject().value("pricePerCharge").toString());
+        hybridCar[i].append(val.toObject().value("batteryLife").toString());
+        hybridCar[i].append(val.toObject().value("kmPerLitre").toString());
+        hybridCar[i].append(val.toObject().value("electricPriceModifier").toString());
+        hybridCar[i].append(val.toObject().value("gasPriceModifier").toString());
+        qDebug() << hybridCar[i];
+        i++;
+
+    }
+
+
+    QFile inFile7("C:/Users/shahz/Desktop/RenTogether/user.json");
+    inFile7.open(QIODevice::ReadOnly | QIODevice::Text);
+    data = inFile7.readAll();
+    inFile7.close();
+
+
+    doc = QJsonDocument::fromJson(data, &errorPtr);
+    if (doc.isNull()) {
+       qDebug() << "Parse failed";
+   }
+    rootObj = doc.object();
+    ptsArray = rootObj.value("user").toArray();
+
+    i = 0;
+    foreach(const QJsonValue & val, ptsArray){
+        user[i].append(val.toObject().value("userID").toString());
+        user[i].append(val.toObject().value("password").toString());
+        qDebug() << user[i];
+        i++;
+    }
+
+      return 0;
+}
