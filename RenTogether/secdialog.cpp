4ce9c0a6c5754271d59eaf1a83dfdc70ca2d42ea
xinyi <90232197+txyxinyi@users.noreply.github.com>
2022-03-30 21:12:38 +0800

    updated GUI and display vehicle details

--- RenTogether/secdialog.cpp
+++ RenTogether/secdialog.cpp
@@ -6,6 +6,27 @@ SecDialog::SecDialog(QWidget *parent) :
     ui(new Ui::SecDialog)
 {
     ui->setupUi(this);
+
+    ui->Brand->setText("Scorpio Electric");
+    ui->SeaterNumber->setText("");
+    ui->Model->setText("X1");
+    ui->BatteryLife->setText("100");
+    ui->Mileage->setText("");
+    ui->BasePrice->setText("40");
+
+    ui->Brand->setText("Energica");
+    ui->SeaterNumber->setText("");
+    ui->Model->setText("EGO+");
+    ui->BatteryLife->setText("100");
+    ui->Mileage->setText("");
+    ui->BasePrice->setText("45");
+
+    ui->Brand->setText("Harley-Davidson");
+    ui->SeaterNumber->setText("");
+    ui->Model->setText("LiveWire");
+    ui->BatteryLife->setText("100");
+    ui->Mileage->setText("");
+    ui->BasePrice->setText("60");
 }
 
 SecDialog::~SecDialog()
