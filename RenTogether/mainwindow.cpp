847551c161204592e42791b0637daa6af1ee6f9e
shahzad1999 <shahzad1999@hotmail.com>
2022-04-07 17:22:12 +0800

    error handling for user login

--- RenTogether/mainwindow.cpp
+++ RenTogether/mainwindow.cpp
@@ -10,6 +10,9 @@
 
 #include <QVector>
 
+const QString USER_ERROR = "No user found";
+const QString LOGIN_ERROR = "Incorrect Login Credentials";
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -26,39 +29,55 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_loginButton_clicked()
 {
-    QString username = ui->userInput->text();
 
-    // convert password input to std::string to hash and then converts it back to QString
-    QString hash = QString::fromStdString(sha256(ui->passwdInput->text().toLocal8Bit().constData()));
-    QVector<Customer*> custList = reader.getCustomerList();
-    int userIndex = -1;
-    for (int i = 0; i < custList.size(); i++) {
-        if (custList[i]->getUsername() == username) {
-            userIndex = i;
-            break;
+    // try to get user inputs and validate
+    try {
+
+        QString username = ui->userInput->text();
+
+        // convert password input to std::string to hash and then converts it back to QString
+        QString hash = QString::fromStdString(sha256(ui->passwdInput->text().toLocal8Bit().constData()));
+        QVector<Customer*> custList = reader.getCustomerList();
+        int userIndex = -1;
+        for (int i = 0; i < custList.size(); i++) {
+            if (custList[i]->getUsername() == username) {
+                userIndex = i;
+                break;
+            }
         }
-    }
 
-    if (userIndex != -1) {
-        //implement hash function later
-        if(custList[userIndex]->getHash() == hash) {
+        if (userIndex != -1) {
 
-            reader.setCurrentCustomer(custList[userIndex]);
-            QString fn = reader.getCurrentCustomer()->getFirstName();
-            QString ln = reader.getCurrentCustomer()->getLastName();
-            QMessageBox::information(this, "Login", "Welcome! " + fn + " " + ln);
+            if(custList[userIndex]->getHash() == hash) {
 
-            hide();
-            secdialog = new SecDialog(this, &reader);
-            secdialog->show();
+                reader.setCurrentCustomer(custList[userIndex]);
+                QString name = reader.getCurrentCustomer()->getFirstName() + " " + reader.getCurrentCustomer()->getLastName();
+                throw(name);
+            }
+            else {
+                // if credential is invalid, throw 1
+                throw(1);
+            }
         }
         else {
-            QMessageBox::warning(this,"Login", "Incorrect Login Credentials");
+            // if invalid user, throw 2
+            throw(2);
+        }
+
+    }catch (QString name) { // catch the name and display welcome message and show rental list
+        QMessageBox::information(this, "Login", "Welcome! " + name);
+        hide();
+        secdialog = new SecDialog(this, &reader);
+        secdialog->show();
+    }catch (int error) { // catch all int throws. If 1, display LOGIN_ERROR, if 2 display USER_ERROR
+        if(error == 1){
+            QMessageBox::warning(this,"Login", LOGIN_ERROR);
+        }else{
+            QMessageBox::warning(this,"Login", USER_ERROR);
         }
     }
-    else {
-        QMessageBox::warning(this,"Login", "No user found");
-    }
+
+
 }
 
 jsonReader &MainWindow::getReader()
